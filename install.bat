5f9882e5269855975f2e53b3820cb19c17b847c5
rapid <rapidslayer101@gmail.com>
2022-07-20 23:20:35 +0100

    Testing launcher.py removal and python-venv -Attempt 2

--- install.bat
+++ install.bat
@@ -2,6 +2,6 @@ echo Downloading files
 git clone https://github.com/rapidslayer101/Rdisc
 cd Rdisc
 echo Unzipping python virtual environment
-Expand-Archive venv.zip
+tar -xf venv.zip
 echo Starting client
 venv/Scripts/python.exe rdisc.py
